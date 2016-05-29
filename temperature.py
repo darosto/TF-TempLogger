8d2ea67fa19c74255f75ff02720427f3f11b1281
gus484 <gustav484@web.de>
2016-05-29 17:06:19 +0200

    code style

--- temperature.py
+++ temperature.py
@@ -21,6 +21,9 @@ class EthTemperature:
 	DB_PASS = "DBPASS"
 	TB_OFFSET = 0 * 100
 	PTC_OFFSET = 2.7 * 100
+	CISTERN_MAX = 1000
+	CISTERN_MIN = 2500
+
 
 	def __init__(self):
 		self.temp 	= None
@@ -58,6 +61,7 @@ class EthTemperature:
 			print "Could not connect to tinkerforge"
 			self.ipcon = None
 
+
 	def release(self):
 		if self.ipcon is not None:
 			self.ipcon.disconnect()
@@ -68,7 +72,11 @@ class EthTemperature:
 		if self.cnx is not None:
 			self.cnx.close()
 
+
 	def connect_db(self):
+		'''
+			Connect to database
+		'''
 		try:
 			self.cnx = mysql.connector.connect(user=EthTemperature.DB_USER, password=EthTemperature.DB_PASS,
 				host=EthTemperature.DB_HOST, database=EthTemperature.DB_NAME)
@@ -79,7 +87,11 @@ class EthTemperature:
 			self.cnx = None
 			self.cursor = None
 
+
 	def read_config(self):
+		'''
+			Read config parameters from database
+		'''
 		if self.cursor is None:
 			return
 
@@ -89,7 +101,11 @@ class EthTemperature:
 		for (cfg_key, cfg_value) in self.cursor:
 			self.config[cfg_key] = cfg_value
 
+
 	def get_temperature(self, sensor):
+		'''
+			Get temperature from selected sensor
+		'''
 		if sensor == "PTC":
 			if self.ptc is None:
 				return 0
@@ -101,7 +117,8 @@ class EthTemperature:
 			else:
 				return self.temp.get_temperature() + EthTemperature.TB_OFFSET
 
-	def _flushCisternSensor(self):
+
+	def _flush_cistern_sensor(self):
 		msg_len = 1
 		while msg_len != 0:
 			us_msg = self.rs232.read()
@@ -109,7 +126,11 @@ class EthTemperature:
 		time.sleep(0.5)
 		return
 
+
 	def get_cistern_level(self):
+		'''
+			Read the ultrasonic sensor from cistern
+		'''
 		# check if bricklet connected
 		if self.rs232 == None:
 			return 0
@@ -133,15 +154,49 @@ class EthTemperature:
 					break
 				i = i + 1
 
-			dist = ''.join(us_msg[start:end]) # in mm
-			dist = int(dist)
+			if start is not None:
+				dist = ''.join(us_msg[start:end]) # in mm
+				dist = int(dist)
 
 		return dist
 
 
+	def calc_cistern_water_volume(self, dist):
+		raise Exception("Not implemented!")
 
-	def write_temperature(self):
 
+	def log_cistern_level(self):
+		'''
+			Log cistern level into database
+		'''
+		if self.cursor == None or self.cnx == None:
+			return
+
+		dist = get_cistern_level()
+
+		# check if value is valid
+		if dist == 0 or dist == 5000:
+			return
+
+		sql = ("INSERT INTO tl_measurements_cistern "
+               "(measurement_date, level) "
+               "VALUES (%(a)s, %(b)s")
+
+		args = { 'a' : self.now.strftime('%Y-%m-%d %H:%M:%S'), 'b' : dist}
+
+		self.cursor.execute(sql,args)
+
+		# insert new cistern level
+
+		mid = self.cursor.lastrowid
+
+		self.cnx.commit()
+
+
+	def log_temperature(self):
+		'''
+			Log temperature into database
+		'''
 		if self.cursor == None or self.cnx == None:
 			return
 
@@ -162,13 +217,21 @@ class EthTemperature:
 
 		self.cnx.commit()
 
+
 	def set_relay(self,state):
+		'''
+			Switch the relay
+		'''
 		if self.dr is None or isinstance(state,bool) is False:
 			return
 
 		self.dr.set_selected_state(self.relay, state)
 
+
 	def check_temperature(self):
+		'''
+			Check if temperature under min or over max and switch the relay.
+		'''
 		if self.ptc is None:
 			return
 
@@ -179,9 +242,12 @@ class EthTemperature:
 		elif temp_inside/100.0  > float(self.config["max_temp"]):
 			self.set_relay(False)
 
-	# callback handles device connections and configures possibly lost
+
 	def cb_enumerate(self, uid, connected_uid, position, hardware_version,
 				firmware_version, device_identifier, enumeration_type):
+		'''
+			Callback handles device connections and configures possibly lost
+		'''
 		if enumeration_type == IPConnection.ENUMERATION_TYPE_CONNECTED or enumeration_type == IPConnection.ENUMERATION_TYPE_AVAILABLE:
 			# enumeration is for temperature bricklet
 			if device_identifier == Temperature.DEVICE_IDENTIFIER:
@@ -208,8 +274,11 @@ class EthTemperature:
 				self.rs232.set_configuration(BrickletRS232.BAUDRATE_9600, BrickletRS232.PARITY_NONE, BrickletRS232.STOPBITS_1,
 					BrickletRS232.WORDLENGTH_8, BrickletRS232.HARDWARE_FLOWCONTROL_OFF, BrickletRS232.SOFTWARE_FLOWCONTROL_OFF)
 
-	# check if all sensors for temperature control connected
+
 	def is_ready(self, mode):
+		'''
+			Check if all sensors for chossen mode connected
+		'''
 		if mode == 'temperature':
 			if self.dr is not None and self.ptc is not None:
 				return True
@@ -218,12 +287,16 @@ class EthTemperature:
 				return True
 		return False
 
-	# callback handles reconnection of ip connection
+
 	def cb_connected(self, connected_reason):
+		'''
+			Callback handles reconnection of ip connection
+		'''
 		# enumerate devices again. if we reconnected, the bricks/bricklets
 		# may have been offline and the configuration may be lost.
 		# in this case we don't care for the reason of the connection
 		self.ipcon.enumerate()
+
 
 if __name__ == "__main__":
 	et = EthTemperature()
@@ -239,7 +312,7 @@ if __name__ == "__main__":
 
 	if et.is_ready('temperature'):
 		if et.now.minute == 0: # log temp every hour
-			et.write_temperature()
+			et.log_temperature()
 		et.check_temperature()
 
 	if et.is_ready('cistern'):
